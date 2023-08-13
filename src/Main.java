a5c08f12742bcf8c4ab5badd720e521037ae239e
chauhant368 <chauhant368@gmail.com>
2023-08-13 18:14:11 +0530

    refactored variable

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int count= 1; count <= 5; count++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println("count = " + count);
         }
     }
 }
